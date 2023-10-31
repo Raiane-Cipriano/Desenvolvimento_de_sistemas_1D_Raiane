b1c72cfed28a35209ba4ae3b907d84392f936048
Raiane-Cipriano <144157950+Raiane-Cipriano@users.noreply.github.com>
2023-10-31 14:10:58 -0300

    Lista 01

---
+++ 11.py
@@ -0,0 +1,9 @@
+n1= int(input("digite seu numero"))
+n2= int(input("digite seu numero"))
+n3=int(input("digite seu nomero"))
+n4=int(input("digite seu numero")) 
+              
+              
+              
+              
+            

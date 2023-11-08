df1d39f278629a642571528f04f02896145374f8
Raiane-Cipriano <144157950+Raiane-Cipriano@users.noreply.github.com>
2023-11-08 13:48:21 -0300

    Lista 1

--- 11.py
+++ 11.py
@@ -1,8 +1,14 @@
 n1= int(input("digite seu numero"))
 n2= int(input("digite seu numero"))
 n3=int(input("digite seu nomero"))
-n4=int(input("digite seu numero")) 
-              
+p1=(n1*2)*(n2/2)
+p2=(n1*3)+n3
+p3= n3**3
+print("o resultado e",p1)
+print("o resultado e",p2)
+print("o resultado e",p3)
+
+            
               
               
               
---
+++ 13.py
@@ -0,0 +1,5 @@
+a=float(input("digite sua altura"))
+p_h=(72.7*a)-58
+p_m=(62.1*a)-44.7
+print("o pesso ideal para sua altura se você for homem é",p_h,"kg")
+print("o pesso ideal para sua altura se voce for mulher é",p_h,"kg")

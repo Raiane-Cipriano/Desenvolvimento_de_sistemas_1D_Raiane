df3740741e4d61bfc40dcf34cffcef6ac4e2a430
Raiane-Cipriano <144157950+Raiane-Cipriano@users.noreply.github.com>
2023-11-20 15:00:18 -0300

    Lista 1

--- 12.py
+++ 12.py
@@ -1,1 +1,3 @@
-
+a=float(input("digite sua autura  em metros"))
+p_f=(72.7*a)-58
+print("seu pesso ifeui e",p_f)
--- 16.py
+++ 16.py
@@ -1,4 +1,4 @@
 m=float(input("quantos metros voce quer pintar"))
 letras=m/(18*3)
 preco=letras*80
-print("voce precisará comprar ",latas,"latas que cutarão",preco)
+print("voce precisará comprar ",letras,"latas que cutarão",preco)
